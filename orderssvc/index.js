5f71811aaf65310903b0345dd90fbf0155ae6073
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2019-12-16 16:02:06 +0100

    dot env

--- orderssvc/index.js
+++ orderssvc/index.js
@@ -2,25 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const AWS = require('aws-sdk');
 
+// Configure the region 
 AWS.config.update({region: 'eu-central-1'});
 
+// Create an SQS service object
 const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
-const queueUrl = "SQS_QUEUE_URL";
+const queueUrl = process.env.SQS_QUEUE_URL;
+console.log("url queee", queueUrl)
 
 const port = process.argv.slice(2)[0];
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/index', (req, res) => {
+app.get('/', (req, res) => {
     res.send("Welcome to NodeShop Orders.")
 });
 
-console.log(`Orders service listening on port ${port}`);
-app.listen(port);
+app.post('/order', (req, res) => {
 
-
-app.post('/get',(req, res) => {
     let orderData = {
         'userEmail': req.body['userEmail'],
         'itemName': req.body['itemName'],
@@ -53,17 +53,18 @@ app.post('/get',(req, res) => {
         QueueUrl: queueUrl
     };
 
-    // Send the order data to the SQS queue
+    // send the order data to the SQS queue
     let sendSqsMessage = sqs.sendMessage(sqsOrderData).promise();
-
+    
     sendSqsMessage.then((data) => {
         console.log(`OrdersSvc | SUCCESS: ${data.MessageId}`);
         res.send("Thank you for your order. Check you inbox for the confirmation email.");
     }).catch((err) => {
         console.log(`OrdersSvc | ERROR: ${err}`);
-        // Send email to emails API
-        res.send("We ran into an error. Please try again.");
+        // send email to emails API
+        res.send("We ran into an error. Please try again.");        
     });
+});
 
-
-})
+console.log(`Orders service listening on port ${port}`);
+app.listen(port);
