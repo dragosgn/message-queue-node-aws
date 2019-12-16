f30cadfaed3af1c100fbcd0cc68d62e207b92cb3
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2019-12-16 16:36:52 +0100

    working demo

--- emailssvc/index.js
+++ emailssvc/index.js
@@ -7,21 +7,22 @@ AWS.config.update({region: 'eu-central-1'});
 const queueUrl = process.env.SQS_QUEUE_URL;
 
 
-let transport = nodemailer.createTransport({
-    host: 'smtp.googlemail.com',
-    port: 587,
-    auth: {
-        user: 'Email address',
-        pass: 'Password'
-    }
-});
+console.log("passs", process.env.EMAIL_PASS)
 
+
+let transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "b83fd7215e458d",
+      pass: "22fc757f55b6cd"
+    }
+  });
 
 function sendEmail(message) {
     let sqsMessage = JSON.parse(message.Body);
-
     const emailMessage = {
-        from: 'sender_email_adress',    // Sender address
+        from: 'dragosgn@gmail.com',    // Sender address
         to: sqsMessage.userEmail,     // Recipient address
         subject: 'Order Received | NodeShop',    // Subject line
         html: `<p>Hi ${sqsMessage.userEmail}.</p. <p>Your order of ${sqsMessage.itemsQuantity} ${sqsMessage.itemName} has been received and is being processed.</p> <p> Thank you for shopping with us! </p>` // Plain text body
@@ -41,7 +42,7 @@ function sendEmail(message) {
 const app = Consumer.create({
     queueUrl: queueUrl,
     handleMessage: async (message) => {
-        sendMail(message);
+        sendEmail(message);
     },
     sqs: new AWS.SQS()
 });
